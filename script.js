fd14f5d79edb4375e9bd40f659e2d08295be1833
gcetj <85449713+gcetj@users.noreply.github.com>
2021-06-08 23:16:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,164 @@
+//const deptElement = document.getElementById("dept");
+const subCodesElement = document.getElementById("subCode");
+const yearElement = document.getElementById("year");
+const errElement = document.getElementById("errMessage");
+const uploadMessageElement = document.getElementById("uploadMessage");
+const regulationElement = document.getElementById("regulation");
+const form = document.getElementById('form');
+
+var d = new Date();
+
+const subCodes = {
+    "2013": ["HS6151","MA6151","PH6151","CY6151","GE6151","GE6152","HS6251","MA6251","PH6251","CY6251","EC6201","EE6201","MA6351","EE6352","EC6301","EC6302","EC6303","EC6304","MA6451","EC6401","EC6402","EC6403","EC6404","EC6405","EC6501","EC6502","EC6503","GE6351","EC6504","MG6851","CS6303","CS6551","EC6601","EC6602","EC6701","EC6702","EC6703","EC6801","EC6802","EC6001","EC6004","IT6005","EC6009","EC6013","EC6016","EC6018","GE6075","GE6083","EC6019","CS6701","GE6757"],
+    "2017": ["HS8151","MA8151","PH8151","CY8151","GE8151","GE8152","HS8251","MA8251","PH8253","BE8254","EC8251","EC8252","MA8352","EC8393","EC8351","EC8352","EC8392","EC8391","MA8451","EC8452","EC8491","EC8451","EC8453","GE8291","EC8501","EC8553","EC8552","EC8551","EC8691","EC8095","EC8652","MG8591","EC8651","EC8701","EC8751","EC8791","EC8702","GE8077","EC8073","EC8004","GE8071","EC8093","GE8076","EC8094"]
+};
+
+function changeRegulation() {
+    subCodesElement.innerHTML = '';
+
+    let optionsInsert = '';
+    //console.log(optionsInsert);
+    let option = document.createElement('option');
+    option.text = '--Select one--';
+    option.value = "none";
+    subCodesElement.append(option);
+    //optionsInsert.concat(option);
+    //console.log(optionsInsert);
+
+    let subjectCodes = subCodes[regulationElement.value].sort();
+
+    for(let i = 0; i < subjectCodes.length; i++) {
+        option = document.createElement('option');
+        option.text = subjectCodes[i];
+        option.value = subjectCodes[i];
+        subCodesElement.append(option);
+        optionsInsert.concat(option);
+    }
+    // console.log(optionsInsert);
+    // subCodesElement.innerHTML = optionsInsert;
+}
+
+function deptChange(e) {
+    subCodesElement.innerHTML = '';
+    //let department = deptElement.value;
+    
+    let option = document.createElement('option');
+    option.text = "--Select one--";
+    option.value = "none";
+    subCodesElement.append(option);
+
+    let subjectCodes = subCodes.sort();
+
+    for(let i = 0; i < subjectCodes.length; i++) {
+        let option = document.createElement('option');
+        option.text = subjectCodes[i];
+        option.value = subjectCodes[i];
+        subCodesElement.append(option);
+    }
+}
+
+const checkInputs = () => {
+    if (subCodesElement.value == "none" || yearElement.value == "none") {
+        alert("Invalid year or subject code");
+        return false;
+    }
+    return true;
+}
+
+const checkTime = () => {
+    console.log("Checking the time");
+    if (!(d.getHours() >= 13 && d.getHours() <= 14 || d.getHours() >= 18 && d.getHours() <= 22)) {
+        errElement.style.display = "block";
+        enabled = false;
+        return false;
+    }
+    return true;
+}
+
+const checkFileNaming = (filename) => {
+    let pdfCheck = filename.split(".");
+    if(pdfCheck[1] != "pdf" && pdfCheck[1] != "PDF") {
+        alert("Only pdf files are accepted");
+        return false;
+    }
+
+    let filenaming = filename.split("-");
+    console.log(filenaming);
+    if (filenaming.length != 2 || filenaming[0].length != 12 || filenaming[1].length != 10) {
+        alert("File name is not proper");
+        alert("File name should in the format [Reg.No]-[Sub.Code] (all uppercase)");
+        return false;
+    }
+
+    return true;
+}
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    
+    console.log(checkInputs());
+
+    if (!checkInputs()) {
+        return;
+    }   
+
+    if (!confirm("Sure to submit?")) {
+        return;
+    }
+
+    if (form.filename.value.length != 12) {
+        alert("Check your register number");
+        return;
+    }
+
+    // if (!checkTime()) {
+    //     alert("Answer Submission Time exceeded! Contact your Supervisor");
+    //     return;
+    // }
+    const file = form.file.files[0];
+    const fr = new FileReader();
+    var d = new Date();
+
+    try {
+        fr.readAsArrayBuffer(file);
+    } catch(err) {
+        alert("Please make sure you selected the correct file or contact your supervisor");
+        uploadMessageElement.innerHTML = 'UPLOADED SUCCESSFULLY';
+        uploadMessageElement.style.display = 'block';
+    }
+
+    fr.onload = f => {
+
+        if (!checkFileNaming(file.name)) {
+            return;
+        }
+        
+        uploadMessageElement.innerHTML = "Uploading... Please Wait!"
+        uploadMessageElement.style.display = 'block';
+
+        let fileName = file.name;
+
+        let url = "https://script.google.com/macros/s/AKfycbzyRIFz2LrmvrMTdxWyayTnoAnk8U6HAnq9GlaVZo_BF24JjDb4tOO9y-kl1hoGXDNa/exec";
+
+        const qs = new URLSearchParams({filename: fileName, mimeType: file.type, subCode: form.subCode.value});
+        console.log(`${url}?${qs}`);
+        fetch(`${url}?${qs}`, {method: "POST", body: JSON.stringify([...new Int8Array(f.target.result)])})
+        .then(res => res.json())
+        .then(e => {
+            console.log(e);
+            if (e.commonFolder) {
+                alert("It seems like your file went to the wrong folder. Contact the supervisor");
+            }
+            alert("File uploaded successfully!");
+            form.reset();
+            uploadMessageElement.innerHTML = 'UPLOADED SUCCESSFULLY';
+            uploadMessageElement.style.display = 'block';
+        })  // <--- You can retrieve the returned value here.
+        .catch(err => {
+            console.log(err);
+            uploadMessageElement.innerHTML = 'UPLOADING FAILED';
+            alert("Something went Wrong! Please Try again!");
+            uploadMessageElement.style.display = 'block';
+        });
+    }
+});
